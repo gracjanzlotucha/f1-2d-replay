e221e0c7f9c9dc6386379f526e1ab887e77601ee
gracjanzlotucha <gracjanzlotucha1@gmail.com>
2026-02-25 17:10:55 +0100

    Play badge spacing fixes

--- static/app.js
+++ static/app.js
@@ -1286,10 +1286,8 @@ function renderRaceInsights() {
           <div class="ric-drivers${overlapClass}">${itemsHtml}</div>
           <span class="ric-lap">
             <span class="ric-lap-icon">${PLAY_SVG}</span>
-            <span class="ric-lap-labels">
-              <span class="ric-lap-text">Lap ${ins.lap}</span>
-              <span class="ric-lap-play">Play</span>
-            </span>
+            <span class="ric-lap-text">Lap ${ins.lap}</span>
+            <span class="ric-lap-play">Play</span>
           </span>
         </div>
         <div class="ric-details">
